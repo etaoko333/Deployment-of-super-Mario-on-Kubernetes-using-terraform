c384657ec2221b79a65f8b49cb69179e55884ef5
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2025-01-07 18:56:50 +0000

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario12bucket" # Replace with your actual S3 bucket name
+    bucket = "mario-super" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-1"
   }
 }
